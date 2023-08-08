5eee2cf57543d69e3545b8004458f904fa013a79
michelmnds <m1tchofc@gmail.com>
2023-08-08 18:37:05 +0100

    feat: entities criadas

---
+++ src/entities/car_image.entitie.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Car } from "./car.entitie";
+
+@Entity("car_images")
+export class CarImages {
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+  @Column()
+  url: string;
+
+  @ManyToOne(() => Car, (car) => car.carImages)
+  car: Car;
+}
---
+++ src/entities/car_user_comment.entities.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Car } from "./car.entitie";
+import { User } from "./user.entitie";
+
+@Entity("car_user_comments")
+export class CarUserComments {
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+  @Column()
+  comment: string;
+
+  @ManyToOne(() => Car, (car) => car.comments)
+  car: Car;
+  @ManyToOne(() => User, (user) => user.comments)
+  user: User;
+}
