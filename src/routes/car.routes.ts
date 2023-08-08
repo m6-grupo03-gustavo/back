a626cd49700f84785472c61aa6f23424e20353ca
WillianMascarenhas <masca.will@gmail.com>
2023-08-08 15:45:32 -0300

    feat: car routes

---
+++ src/routes/car.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+export const CarRouter = Router()
+CarRouter.get("")
+CarRouter.get("/:id")
+CarRouter.post("")
+CarRouter.patch("/:id")
+CarRouter.delete("/:id")
