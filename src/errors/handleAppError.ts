de17caf1e33428e4d833f4cea95fd7fbb3627ba9
WillianMascarenhas <masca.will@gmail.com>
2023-08-08 15:36:09 -0300

    feat: appError, middleware to verify resquest data, delete/update service

---
+++ src/errors/handleAppError.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "./app.error";
+import { ZodError } from "zod";
+
+export const handlerAppError = (
+  error: Error,
+  req: Request,
+  res: Response,
+  _: NextFunction
+) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({ message: error.message });
+  }
+  if (error instanceof ZodError) {
+    return res.status(400).json({ message: error.flatten().fieldErrors });
+  }
+  return res.status(500).json({ message: "internal server error" });
+};
---
+++ src/middlewares/ensureRequestData.middleware.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from "express";
+import { ZodTypeAny } from "zod";
+
+export const EnsureRequestData = (schema: ZodTypeAny) => (req:Request, res:Response, next: NextFunction) =>{
+    const validatedData = schema.parse(req.body)
+
+    req.body = validatedData
+
+    return next()
+}
