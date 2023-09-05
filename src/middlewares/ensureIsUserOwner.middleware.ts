d7916a45c0df965b0aa7615c1205c7ed57709c8f
michelmnds <m1tchofc@gmail.com>
2023-09-05 14:02:32 +0100

    feat: additions to patchUserController

--- src/middlewares/ensureIsUserOwner.middleware.ts
+++ src/middlewares/ensureIsUserOwner.middleware.ts
@@ -6,52 +6,54 @@ import { AppError } from "../errors/app.error";
 import { Repository } from "typeorm";
 import { CarUserComments } from "../entities/car_user_comment.entities";
 
-export const ensureIsUserOwner = async ( req: Request, res: Response, next: NextFunction ) => {
+export const ensureIsUserOwner = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.baseUrl === "/car") {
+    const userId = parseInt(res.locals.userId);
+    const carId = parseInt(req.params.id);
 
-    if(req.baseUrl === "/car"){
-        const userId = parseInt(res.locals.userId)
-        const carId = parseInt(req.params.id)
+    const userRepository = AppDataSource.getRepository(User);
+    const findUSer = await userRepository.findOneBy({ id: userId });
 
-
-        const userRepository = AppDataSource.getRepository(User)
-        const findUSer = await userRepository.findOneBy({id:userId})
-
-    const carRepository = AppDataSource.getRepository(Car)
+    const carRepository = AppDataSource.getRepository(Car);
 
     const findCar = await carRepository.findOne({
-        where:{
-            id: carId
-        },
-        relations: ["user"]
-    })
-    if(findCar?.user.id !== findUSer!.id){
-        throw new AppError("You must be user owner", 403)
+      where: {
+        id: carId,
+      },
+      relations: ["user"],
+    });
+    if (findCar?.user.id !== findUSer!.id) {
+      throw new AppError("You must be user owner", 403);
     }
 
-    return next()
-}else if(req.baseUrl === "/user"){
-    const userTokenId = parseInt(res.locals.userId)
-    const userParamsId = parseInt(req.params.id)
+    return next();
+  } else if (req.baseUrl === "/user") {
+    const userTokenId = parseInt(res.locals.userId);
+    const userParamsId = parseInt(req.params.id);
 
-    if(userTokenId !== userParamsId){
-        throw new AppError("You must be user owner", 403)
+    if (userTokenId !== userParamsId) {
+      throw new AppError("You must be user owner", 403);
     }
-    return next()
-}else if(req.baseUrl === "/comments"){
-    const userTokenId = parseInt(res.locals.userId)
-    const id = parseInt(req.params.commentId)
-    const commentRepository: Repository<CarUserComments> = AppDataSource.getRepository(CarUserComments);
+    return next();
+  } else if (req.baseUrl === "/comments") {
+    const userTokenId = parseInt(res.locals.userId);
+    const id = parseInt(req.params.commentId);
+    const commentRepository: Repository<CarUserComments> =
+      AppDataSource.getRepository(CarUserComments);
 
-  const comment = await commentRepository.findOne({
-    where: {
-      id: id,
-    },
-  });
+    const comment = await commentRepository.findOne({
+      where: {
+        id: id,
+      },
+    });
 
-    if(userTokenId !== Number(comment?.user)){
-        throw new AppError("You must be user owner", 403)   
+    if (userTokenId !== Number(comment?.user)) {
+      throw new AppError("You must be user owner", 403);
     }
-    return next()
-}
-
-}
+    return next();
+  }
+};
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -17,9 +17,19 @@ export const userRoutes = Router();
 
 userRoutes.post("", createUserController);
 userRoutes.get("/", getUserController);
-userRoutes.get("/:id",ensureIsAuthMiddleware, getUserControllerById);
-userRoutes.patch("/:id",ensureIsAuthMiddleware, ensureIsUserOwner, patchUserController);
-userRoutes.delete("/:id",ensureIsAuthMiddleware,ensureIsUserOwner, deleteUserController);
+userRoutes.get("/:id", ensureIsAuthMiddleware, getUserControllerById);
+userRoutes.patch(
+  "/:id",
+  ensureIsAuthMiddleware,
+  ensureIsUserOwner,
+  patchUserController
+);
+userRoutes.delete(
+  "/:id",
+  ensureIsAuthMiddleware,
+  ensureIsUserOwner,
+  deleteUserController
+);
 
 userRoutes.post("/resetUserPassword", resetUserPasswordEmailController);
 userRoutes.patch("/resetUserPassword/:token", resetUserPasswordController);
