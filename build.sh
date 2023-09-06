d759ad4a68b68eb601b1fb599f69cecbb63b3c03
carolinalmeidas <carolinalmeida.s@hotmail.com>
2023-09-06 15:37:43 -0300

    fix: deploy vercel

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -o errexit
 
 npm install
 yarn run build
-npm run typeorm migration:run -- -d dist/data-source
+npm run typeorm migration:run -- -d scr/data-source
--- dist/routes/login.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loginRoutes = void 0;
-const express_1 = require("express");
-const login_controller_1 = require("../controllers/login.controller");
-exports.loginRoutes = (0, express_1.Router)();
-exports.loginRoutes.post("", login_controller_1.createTokenController);
