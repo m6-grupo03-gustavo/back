c9a321bb7f008c4c3a7b12b945c978799952868a
carolinalmeidas <carolinalmeida.s@hotmail.com>
2023-09-06 16:21:50 -0300

    fix: deploy render

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,6 +6,21 @@ const DataSourceConfig = (): DataSourceOptions => {
   const entitiesPath = path.join(__dirname, "entities/**.{js,ts}");
   const migrationsPath = path.join(__dirname, "migrations/**.{js,ts}");
 
+  const nodeEnv: string | undefined = process.env.NODE_ENV;
+
+    if (nodeEnv === "production") {
+        if (!process.env.DATABASE_URL) {
+            throw new Error("Env DATABASE_URL does not exist");
+        }
+
+        return {
+            type: "postgres",
+            url: process.env.DATABASE_URL,
+            entities: [entitiesPath],
+            migrations: [migrationsPath],
+        };
+    }
+
     if(!process.env.DATABASE_URL){
         throw new Error("Env DATABASE_URL does not exists")
     }
